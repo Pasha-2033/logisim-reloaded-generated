ce6cc386ed2cb422da791047c6c1455850965ed2
Павел Соколов <pablo.sokolov@mail.ru>
2021-06-13 23:16:08 +0300

    21.06.13.0.2.fixs.pre.frame

--- modules/workenvironment/WorkEnvironmentMain.java
+++ modules/workenvironment/WorkEnvironmentMain.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 import modules.standartcomponent.wires.power;
 import java.awt.Graphics;
 import java.awt.BorderLayout;
@@ -16,41 +17,42 @@ public class WorkEnvironmentMain {
     public Graphics graphics;
     public WorkEnvironmentMain(JFrame mainframe){
         this.mainframe = mainframe;
-        mainframe.add(workplace);
-        workplace.add(componentframe, BorderLayout.CENTER);
-        workplace.add(toolframe, BorderLayout.WEST);
+        mainframe.add(mainworkplace);
+        mainframe.setMinimumSize(new Dimension(500, 500));
+        mainworkplace.add(workplace);
+        workplace.setPreferredSize(new Dimension(500, 500));
         ProjectComponents.add(new power());
-        ProjectComponents.get(0).ScreenLocation = new int[] {0,0};
+        ProjectComponents.get(0).ScreenLocation = new int[] {0, 0};
         ProjectComponents.get(0).Scale = 100;
         ProjectComponents.get(0).ComponentLocation = new int[] {0, 0}; //- проверка относительных координат
+        ProjectComponents.add(new power());
+        ProjectComponents.get(1).ScreenLocation = new int[] {0, 0};
+        ProjectComponents.get(1).Scale = 100;
+        ProjectComponents.get(1).ComponentLocation = new int[] {0, 0}; //- проверка относительных координат
         componentframe.add(ProjectComponents.get(0));
-        workplace.setPreferredSize(new Dimension(500,500));
         componentframe.setBorder(BorderFactory.createLineBorder(Color.black));
-        power i = new power();
-        i.Scale = 100;
-        i.ScreenLocation = new int[] {0,0};
-        i.ComponentLocation = new int[] {0,1};
-        toolframe.add(i);
+        toolframe.add(ProjectComponents.get(1));
         toolframe.setPreferredSize(new Dimension(100,100));
         toolframe.setBorder(BorderFactory.createLineBorder(Color.black));
         toolframe.setVisible(true);
-        componentframe.setLocation(100, 100);
         this.mainframe.pack();
         //update.start();
     }
     public List<Component> Components = new ArrayList<>(Collections.emptyList());
     public List<Component> ProjectComponents = new ArrayList<>(Collections.emptyList());
     public JFrame mainframe;
-    public JPanel workplace = new JPanel(new BorderLayout());
+    public JPanel mainworkplace = new JPanel(new BorderLayout());
     public JPanel componentframe = new JPanel(new BorderLayout());
     public JPanel toolframe = new JPanel(new BorderLayout());
     public JPanel componentdata = new JPanel(new BorderLayout());
+    public JSplitPane workplace = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, toolframe, componentframe);
     public class workplaceupdate extends Thread{
         public workplaceupdate() {
             setDaemon(true);
         }
         public void run(){
             while (true){
+                //repaint() - для всех панелей - особенно для компонента
                 //workplace.setPreferredSize(new Dimension((int) mainframe.getSize().getWidth(), (int) mainframe.getSize().getHeight()));
             }
         }
