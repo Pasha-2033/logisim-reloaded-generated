a7e06773c1279b1945a766f135bb92370480f334
Павел Соколов <pablo.sokolov@mail.ru>
2021-06-15 19:07:49 +0300

    21.06.15.0.2.frame

--- modules/workenvironment/WorkEnvironmentMain.java
+++ modules/workenvironment/WorkEnvironmentMain.java
@@ -1,29 +1,53 @@
 package modules.workenvironment;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JSplitPane;
-import modules.standartcomponent.wires.power;
 import java.awt.Graphics;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+//import javax.swing.border.Border;
+import modules.standartcomponent.wires.power;
 public class WorkEnvironmentMain {
     public int[] ScreenLocation = {0, 0};
     public int Scale = 100;
     public Graphics graphics;
     public WorkEnvironmentMain(JFrame frame){
         //подготовка панелей
+        componentmenu.setBorder(BorderFactory.createLineBorder(Color.black));
+        componentmenu.setLayout(new BoxLayout(componentmenu, BoxLayout.Y_AXIS));
+        componentmenu.setPreferredSize(new Dimension(100,100));
+        componentmenu.setMinimumSize(new Dimension(100,100));
+        componentmenu.add(toolframe);
+        componentmenu.add(componenttree);
+        componentmenu.add(componentdata);
+        componentmenu.add(framesize);
         componentframe.setBorder(BorderFactory.createLineBorder(Color.black));
         componentframe.setBackground(Color.WHITE);
         componentframe.setOpaque(true);
+        componentdata.setBorder(BorderFactory.createLineBorder(Color.black));
+        componentdata.setBackground(Color.WHITE);
+        componentdata.setOpaque(true);
+        componentdata.setPreferredSize(new Dimension(100,100));
+        componenttree.setBorder(BorderFactory.createLineBorder(Color.black));
+        componenttree.setBackground(Color.BLUE);
+        componenttree.setOpaque(true);
+        componenttree.setPreferredSize(new Dimension(100,100));
         toolframe.setBorder(BorderFactory.createLineBorder(Color.black));
         toolframe.setBackground(Color.RED);
         toolframe.setOpaque(true);
         toolframe.setPreferredSize(new Dimension(100,100));
+        toolframe.setMinimumSize(new Dimension(100,100));
+        toolframe.setMaximumSize(new Dimension(100,100));
+        framesize.setBorder(BorderFactory.createLineBorder(Color.black));
+        framesize.setBackground(Color.RED);
+        framesize.setOpaque(true);
+        framesize.setPreferredSize(new Dimension(100,100));
         workplace.setPreferredSize(new Dimension(500, 500));
         mainworkplace.add(workplace);
         //подготовка экрана
@@ -49,22 +73,28 @@ public class WorkEnvironmentMain {
     public List<Component> Components = new ArrayList<>(Collections.emptyList());
     public List<Component> ProjectComponents = new ArrayList<>(Collections.emptyList());
     public JFrame mainframe;
+    public JPanel framesize = new JPanel(new BorderLayout());
     public JPanel mainworkplace = new JPanel(new BorderLayout());
-    public JPanel componentframe = new JPanel(new BorderLayout());
     public JPanel toolframe = new JPanel(new BorderLayout());
     public JPanel componentdata = new JPanel(new BorderLayout());
-    public JSplitPane workplace = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, toolframe, componentframe);
-    public class workplaceupdate extends Thread{
-        public workplaceupdate() {
+    public JPanel componentmenu = new JPanel();
+    public JPanel componenttree = new JPanel(new BorderLayout());
+    public JPanel componentframe = new JPanel(new BorderLayout());
+    public JSplitPane workplace = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, componentmenu, componentframe);
+    public class movescreen extends Thread{
+        public int[] screenlocation;
+        public movescreen(int [] screenlocation) {
             setDaemon(true);
+            this.screenlocation = screenlocation;
         }
         public void run(){
-            while (true){
-                //repaint() - для всех панелей - особенно для компонента
+            for (Component component : ProjectComponents) {
+                component.ScreenLocation = screenlocation;
+                component.repaint();
             }
         }
     }
-    public workplaceupdate update = new workplaceupdate();
+    public movescreen update = new movescreen(ScreenLocation);
 }
 //ProjectComponents.get(0).setBounds(0, 0, 500, 500); - вряд ли понадобится (и строка ниже)
 //ProjectComponents.get(0).setPreferredSize(new Dimension(500,500));
